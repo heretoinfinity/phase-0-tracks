b336fa1b451fc1f0ccfb8ccf80feb7e8f1cecdcf
Thomas <thomasmbise@gmail.com>
2017-06-23 16:33:15 -0500

    Complete decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -30,6 +30,41 @@ def assert_equal(actual_value, expected_value, test_name)
   end
 end
 
+=begin
+
+take string and traverse it character by character
+  for each character, get its index compared to the string
+    "abcdefghijklmnopqrstuvwxyz"
+      if it is a space, do nothing,
+        and move on to the next character
+      If it is not a space,
+        find out what index the character is in "abcdefghijklmnopqrstuvwxyz"
+          subtract 1 from the index
+          change the character in return string based on comparing
+          to this new index
+=end
+def decrypt(encrypted_string)
+  check_string = "abcdefghijklmnopqrstuvwxyz"
+
+  for character in (0...encrypted_string.length)
+    if encrypted_string[character] == " "
+      next
+    else
+      index_of_char = check_string.index(encrypted_string[character])
+
+      # puts "index in main string #{index_of_char}"
+      # puts "character: #{encrypted_string[character]}"
+      # this solves case of 'a' as it will give -1, the last index
+      encrypted_string[character] = check_string[index_of_char - 1]
+    end
+  end
+
+  return encrypted_string
+end
+
+# puts decrypt('abc de') # => zab cd
+# puts decrypt("bcd") # => "abc"
+# puts decrypt("afe") # => "zed"
 # assert_equal(encrypt("acd z"), "bde aa", "encrypt")
 # assert_equal(encrypt("abc"), "bcd", "encrypt")
 # assert_equal(encrypt("zed"), "afe", "encrypt")
