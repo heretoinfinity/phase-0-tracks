341b3457ff826664ace63f296f6f36acaf4854c9
Thomas <thomasmbise@gmail.com>
2017-06-23 16:40:06 -0500

    Test nested encrypt, decrypt

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,3 +1,17 @@
+=begin
+Method 1: write encrypt method
+Objective: advance each letter of a string one letter forward
+
+Take string
+get length
+
+write a for-loop that traveres through the string, one character at a time
+    If character is a space, leave it as a space.
+    If not, advance it one letter forward in alphabetical order
+Return encrypted string
+
+=end
+
 def encrypt(unencrypted_string)
   # puts "Input: #{unencrypted_string}"
   for character in (0...unencrypted_string.length)
@@ -20,17 +34,10 @@ end
 # puts encrypt("abc") # => "bcd"
 # puts encrypt("zed") # => "afe"
 
-def assert_equal(actual_value, expected_value, test_name)
-  if actual_value == expected_value
-    puts "PASSED: Actual and expected values equal: " \
-    "\"#{expected_value}\""
-  else
-    puts "FAILED \"#{test_name}\": Expected " \
-    "\"#{expected_value}\", but got \"#{actual_value}\""
-  end
-end
 
 =begin
+Method 2: write decrypt method
+Objective: advance each letter of a string one letter forward
 
 take string and traverse it character by character
   for each character, get its index compared to the string
@@ -62,9 +69,28 @@ def decrypt(encrypted_string)
   return encrypted_string
 end
 
-# puts decrypt('abc de') # => zab cd
-# puts decrypt("bcd") # => "abc"
-# puts decrypt("afe") # => "zed"
+
+def assert_equal(actual_value, expected_value, test_name)
+  if actual_value == expected_value
+    puts "PASSED: Actual and expected values equal: " \
+    "\"#{expected_value}\""
+  else
+    puts "FAILED \"#{test_name}\": Expected " \
+    "\"#{expected_value}\", but got \"#{actual_value}\""
+  end
+end
+
 # assert_equal(encrypt("acd z"), "bde aa", "encrypt")
 # assert_equal(encrypt("abc"), "bcd", "encrypt")
 # assert_equal(encrypt("zed"), "afe", "encrypt")
+
+# puts decrypt('abc de') # => zab cd
+# puts decrypt("bcd") # => "abc"
+# puts decrypt("afe") # => "zed"
+
+# nested method works because the word that is encrypted
+# is then passed on to decrypt. Since we are not using
+# the regular string.next method, both methods are based
+# on the characters in  "abcdefghijklmnopqrstuvwxyz" and
+# nothing else
+puts decrypt(encrypt("swordfish")) # => "swordfish"
