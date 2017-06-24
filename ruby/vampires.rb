6dd39e183f9a2d711f66ece985729025883797b6
Thomas <thomasmbise@gmail.com>
2017-06-24 15:44:03 -0500

    Add questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+puts "What is your name?"
+applicant_name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread." \
+" Should we order some for you? "
+serve_garlic? = gets.chomp
+
+puts "Would you like to enroll in the company’s " \
+"health insurance?"
+needs_insurance? = gets.chomp
