e9d1c3790b68b11a468773224afa25e510bb20df
Thomas <thomasmbise@gmail.com>
2017-06-29 15:10:32 -0400

    Complete some methods for strings

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,41 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
 # "zom".<???>
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".reverse.chop.reverse
 # => "he mystery of the missing first letter"
 
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+"z".codepoints
+"z".unpack('U')
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
+# This is the code numbering for the letter 'z' in Unicode 8-bits (UTF-8).
+# The encoding format can be found with 'z'.encoding
 
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4
+
+#count
