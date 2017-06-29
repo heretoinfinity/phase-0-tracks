5b570c6648efea035fc54dd0d9e2a3328aa1c0b7
Thomas <thomasmbise@gmail.com>
2017-06-29 15:11:04 -0400

    Write pseudocode for interior design

---
+++ ruby/hashes.rb
@@ -0,0 +1,19 @@
+=begin
+the client's name, age, number of children, decor theme,
+days for work
+phone
+done work before
+
+prompt user for information
+convert user information into appropriate values, converting
+yes and no to true and false respectively
+When all information is entered, display the hash as is
+
+Display options of what the user can change, and allow
+him to enter corrected information
+
+When done or if user says that there are no changes, display
+the hash again
+
+
+=end
