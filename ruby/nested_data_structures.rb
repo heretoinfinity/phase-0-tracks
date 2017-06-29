2476f07291104e2d7cb49765f4c1e7c5dda9375d
Thomas <thomasmbise@gmail.com>
2017-06-29 17:06:34 -0400

    Create highway hash

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,42 @@
+highway = {
+  east_high_speed_lane: [
+    {
+      car: 'Toyota',
+      passengers: ['Jack', 'Ma'],
+    },
+    {
+      car: 'Honda',
+      passengers: ['Bill', 'Gates']
+    },
+  ],
+
+  east_low_speed_lane: [
+    {
+      car: 'Ford',
+      passengers: ['Warren', 'Buffet', 'Bill', 'Graham'],
+    },
+    {
+      car: 'Chrysler',
+      passengers: ['Anthony']
+    },
+  ],
+
+  west_high_speed_lane: [
+    {
+      car: 'Tesla',
+      passengers: ['Elon', 'Musk', 'Nikola'],
+    },
+
+  ],
+
+  west_low_speed_lane: [
+    {
+      car: 'Mercedes',
+      passengers: ['Angela', 'Gustav'],
+    },
+    {
+      car: 'Kia',
+      passengers: ['Steve', 'Jobs']
+    },
+  ],
+}
