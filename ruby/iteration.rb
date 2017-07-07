b4c34216c5ab9812581c1ef94834c26932bc8c5f
Thomas <thomasmbise@gmail.com>
2017-07-06 22:11:21 -0400

    Complete .each & .map on array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,7 +1,45 @@
-puts "Before block"
+arr = ['Ruby', 'JavaScript', 'HTML', 'CSS']
 
-4.times {print "Ho "}
-print ""
-puts "Merry Christmas"
+hash_ = {
+  'Clooney' => 'ER',
+  'Bale' => 'Batman',
+  'Depp' => 'Pirates of the Carribean',
+  'Dicaprio' => 'Titanic'
+}
 
-puts "After block"
+puts "array before .each"
+puts arr
+
+arr.each { |item|
+  puts "I know #{item}"
+}
+
+puts "array after .each"
+puts arr
+puts "-------------------------"
+
+puts "hash before .each"
+puts hash_
+
+hash_.each { |actor, movie|
+  puts "#{actor} starred in #{movie}"
+}
+
+puts "hash after .each"
+puts hash_
+puts "-------------------------"
+
+puts "array before map"
+puts arr
+
+arr.map! { |item|
+  if item == 'HTML' || item == 'CSS'
+    item = "#{item} is a markup language"
+  else
+    item = "#{item} is a programming language"
+  end
+}
+
+puts "array after map"
+puts arr
+
