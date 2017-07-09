3c30e7007da6ec5ae55a592fc7408884a7d94900
Thomas <thomasmbise@gmail.com>
2017-07-09 13:09:36 -0400

    Add pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,21 @@
+=begin
+1. write methods for the next vowel, next consonant
+  where you compare with strings of just vowels or just
+  consonants, 'aeiou' and 'bcdfghjklm...' - loop with the
+  edge cases 'u' and 'z' to go to 'a' and 'b' respectively.
+  It would be easier to compare with a string and less code
+  than creating 'if-else' or 'case' statements for
+  the vowels and consonants.
+
+2.  take name string and split into first and last names,
+put them in an array, lower cases of both, and swap
+positions -> swap&lower()
+
+3.take each name, split each character
+and get the next letter, capitalize -> getNext()
+
+3. capitalize the first letter for both arrays
+join the 2 array elements and form a string
+->capitalize&Join()
+
+=end
