169116e9e38afec6073a03d02c3bc4e8616e3ede
Thomas <thomasmbise@gmail.com>
2017-07-10 10:27:45 -0400

    Complete iteration drills

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -6,16 +6,45 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # 1. Iterate through the zombie_apocalypse_supplies array using #each,
 # printing each item in the array separated by an asterisk
 # ----
+zombie_apocalypse_supplies.each{ |item|
+  print "#{item}*"
+}
 
 # 2. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies using #each.
 # For instance: are boots in your list of supplies?
 # ----
+def search(arr, str)
+  arr.each{ |item|
+    if item != str
+      next
+    else
+      return true
+    end
+  }
+  return false
+end
+
+# puts search(zombie_apocalypse_supplies,\
+# "binoculars") # => true
+# puts search(zombie_apocalypse_supplies,\
+# "asdf") # => false
 
 # 3. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5, using #each.
 # ----
+# puts "------------"
+# puts "No. 3"
+# puts "------------"
+zombie_apocalypse_supplies.each { |item|
+  if item == "compass"
+    zombie_apocalypse_supplies.delete(item)
+  end
+}
+
+# p " 3. "
+# p zombie_apocalypse_supplies
 
 # 4. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
@@ -25,6 +54,10 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
 # ----
+total_supplies = (zombie_apocalypse_supplies + \
+  other_survivor_supplies).uniq
+
+# p total_supplies
 
 # Hash Drills
 
@@ -41,15 +74,40 @@ extinct_animals = {
 # 1. Iterate through extinct_animals hash using #each, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
-
+extinct_animals.each { |key, value|
+  puts "#{key}-#{value}"
+  puts "*"
+}
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000, using #each.
 # ----
+
+extinct_animals.each { |key, value|
+  if value < 2000
+    extinct_animals.delete(key)
+  end
+}
+
+# print extinct_animals
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # using #each, so they accurately reflect what year the animal went extinct.
 # ----
+
+extinct_animals = {
+  "Passenger Pigeon" => 1914,
+  "Tasmanian Tiger" => 1936,
+  "Eastern Hare Wallaby" => 1890,
+  "Dodo" => 1662,
+  "Pyrenean Ibex" => 2000,
+  "West African Black Rhinoceros" => 2011,
+  "Laysan Crake" => 1923
+}
+
+extinct_animals.each { |key, value|
+  extinct_animals[key] = value - 3
+}
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Build a method  using #each that checks if an animal is in the hash and returns true/false.
@@ -61,8 +119,36 @@ extinct_animals = {
 # Driver code example: is_extinct?(extinct_animals, "Andean Cat")
 # ----
 
+def is_extinct?(extinct_animals, animal)
+  extinct_animals.each { |key, value|
+    if key == animal
+      return true
+    end
+  }
+  return false
+end
+
+# puts is_extinct?(extinct_animals, "Laysan Crake")
+# puts is_extinct?(extinct_animals, "Andean Cat")
+# puts is_extinct?(extinct_animals, "Dodo")
+# puts is_extinct?(extinct_animals, "Saiga Antelope")
+
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find a Ruby Hash built-in method that helps you accomplish this or build
 # your own method using #each
 # ----
+puts "dlels"
+puts "-----"
+
+def remove_animal(animal_hash, animal)
+  animal_hash.each { |key, value|
+  if key = animal
+    animal_hash.delete(key)
+    return [key, value]
+  end
+  }
+  return nil
+end
+
+remove_animal(extinct_animals, "Passenger Pigeon")
