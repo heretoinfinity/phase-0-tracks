9ad453b6d31a37566fbe7ec37de4a7603f7c80a8
Thomas <thomasmbise@gmail.com>
2017-07-12 23:14:48 -0400

    Add characteristics of dog class

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+=begin
+SPECIES ------------------------
+Canis lupus familiaris
+
+CHARACTERISTICS ----------------
+Eyelash length: varies
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+Tail length: varies
+Ear size: varies
+
+BEHAVIOR -----------------------
+Jump
+Bark
+Lick
+Wag tail
+Whimper
+Run
+Mate
+Lap
+=end
