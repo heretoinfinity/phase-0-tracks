6b7e328adfbaa097120a81104ecca7e88ce474a2
Thomas <thomasmbise@gmail.com>
2017-07-13 22:23:24 -0400

    Add driver code to test initialize method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -23,6 +23,18 @@ class Santa
   end
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("chocolate")
+santas = []
+genders = ["male", "female", "gender fluid"]
+ethnicity = ["black", "white", "Hispanic"]
+
+genders.length.times { |item|
+  santas << Santa.new(genders[item], ethnicity[item])
+}
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+puts santas
