4c94be5c5b948cf66e463fa5fcea44dfeffeeba4
Thomas <thomasmbise@gmail.com>
2017-07-15 14:29:46 -0400

    Add pseudocode for game class

---
+++ ruby/game/guess_word.rb
@@ -0,0 +1,29 @@
+=begin
+class name : Guess
+
+Initializing
+ initialize with player 1's word as argument
+  Save the number current guess and max # of guesses
+  Obtain the maximum guess number from the word length
+ keep track of the guesses of player 2 but start off with just blanks
+ boolean to see if the game is over
+
+Allow access to read and write to player 2's guesses, is_over boolean
+
+Only allow read access to player 1's word & maximum number of guesses
+
+Method to check if letter is in word
+  increment guess count by 1
+  IF guess correct
+    find instances in word
+    fill in those instances in player 2's guesses
+    keep track of boolean that guess is correct
+  ELSE
+    keep track of boolean that guess is NOT correct
+
+  IF guess count has reached maximum
+    is_over changed to true
+    return the boolean of whether guess was correct
+
+
+=end
