1eec570fe5dbc9863acb820f914a24248590ced6
Thomas <thomasmbise@gmail.com>
2017-07-21 15:04:51 -0400

    Create 2 car examples

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,3 +11,18 @@ for (var i = 0; i < colors.length; i++) {
 }
 
 console.log(horses)
+
+function Car(model, make, year) {
+  this.model = model;
+  this.make = make;
+  this.year = year;
+  this.rev = function () {console.log('Vroom')}
+}
+
+var car1 = new Car('Civic', 'Honda', '2016');
+console.log(car1);
+car1.rev();
+
+var car2 = new Car('Corolla', 'Toyota', '2008');
+console.log(car2);
+car2.rev();
