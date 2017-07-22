a005d0442955900f2cfe7895f2e6b4b8106f0902
Thomas <thomasmbise@gmail.com>
2017-07-22 16:07:17 -0400

    Add method to generate random words and test it

--- js/algos.js
+++ js/algos.js
@@ -19,4 +19,47 @@ function findLongest(arr){
 
 // DRIVER CODE
 arr = ['one', 'four', 'fifty', 'thirteen'];
-console.log(findLongest(arr));
+console.log('longest word', findLongest(arr));
+
+
+/*
+Start with the 1st key-value pair of the 1st object
+and compare the pair with all the other pairs in the
+2nd object.
+Proceed with doing the same comparison with the next
+pair in the 1st object up until the end
+*/
+
+function compareObjects(obj1, obj2) {
+
+}
+
+/*
+Take number of arguments
+  for each argument
+    generate a random length of the letters
+      select a letter in each iteration from a list of letters
+    add them to list
+return the list
+*/
+
+function generateRandomWords(words) {
+  list = [];
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  for (var i = 0; i < words; i++) {
+    word_length = 1 + Math.round(9 * Math.random());
+    word = ""
+    for (var j = 0; j < word_length; j++) {
+      word += alphabet[Math.round(25*Math.random())];
+    }
+
+    list.push(word);
+  }
+  return list;
+}
+
+// alphabet = "abcdefghijklmnopqrstuvwxyz"
+// x = Math.round(25*Math.random());
+// console.log(x);
+// console.log(alphabet[x]);
+console.log(generateRandomWords(3));
