034b5e449521e8c9b7526ed037bbbb238d48366a
Thomas <thomasmbise@gmail.com>
2017-07-31 16:52:51 -0400

    Add functionality with keyboard & remove bugs

--- databases/to_do_list/to_do_list.rb
+++ databases/to_do_list/to_do_list.rb
@@ -7,8 +7,8 @@ CREATE list_name - to start a new list called list_name
 VIEW list_name - to see tasks in the list including their ids
 ADD list_name task_name - to add task_name to the list
 EDIT list_name task_id new_task_name - to edit task_name to the list
-DELETE_LIST list_name
-DELETE_TASK list_name task_name
+DELETE_LIST list_name - to delete list_name
+DELETE_TASK list_name task_id - to delete task_id in list_name
 EXIT - to exit program
 --------------------------------------------------------------------------
 MESSAGE
@@ -31,14 +31,20 @@ SQL
 end
 
 def view_list(database, list_name)
-  view_list_cmd = 'SELECT * FROM ' + list_name + ';'
+  view_list_cmd = "SELECT * FROM " + list_name + ";"
+
+  # p view_list_cmd
 
   results = database.execute(view_list_cmd)
   # puts results.class
-  puts results
-  # results.each do |row|
-  #   puts "#{row[0]}"
-  # end
+  # p results
+  puts "---------------------------"
+  puts "Viewing '#{list_name}' List"
+  puts "---------------------------"
+  results.each do |row|
+    puts "#{row[0]}| #{row[1]}"
+  end
+  puts "---------------------------"
 end
 
 def add_task(database, list_name, task_name)
@@ -48,17 +54,27 @@ def add_task(database, list_name, task_name)
    SQL
   # p add_task_cmd
   database.execute(add_task_cmd)
+
+  puts "---------------------------"
+  puts "'#{list_name}' List with added task"
+  puts "---------------------------"
+  view_list(database, list_name)
+  puts "---------------------------"
 end
 
 def edit_list(database, list_name, task_id, new_task_name)
-  edit_list_cmd = <<-SQL
-  UPDATE #{list_name}
-  SET task = '#{new_task_name}'
-  WHERE id = #{task_id}
-  ;
-  SQL
-  # puts edit_list_cmd
+  edit_list_cmd = "UPDATE " + list_name + " SET task = '" + \
+  new_task_name + "' WHERE id = " + task_id.to_s + ";"
+
+  # p edit_list_cmd
+
   database.execute(edit_list_cmd)
+
+  puts "---------------------------"
+  puts "'#{list_name}'' List with new edit"
+  puts "---------------------------"
+  view_list(database, list_name)
+  puts "---------------------------"
 end
 
 # def display_lists(database)
@@ -81,37 +97,60 @@ def delete_task(database, list_name, task_id)
     WHERE id IS #{task_id};
   SQL
   database.execute(delete_task_cmd)
+
+  puts "---------------------------"
+  puts "List with task removed"
+  puts "---------------------------"
+  view_list(database, list_name)
+  puts "---------------------------"
 end
 
 db = SQLite3::Database.new("to_do_list.db")
 
-create_list(db, 'school')
-#view_list(db, 'school')
-add_task(db, 'school', 'chemistry')
-#view_list(db, 'school')
-edit_list(db, 'school', 1, 'physics')
-puts "---"
-view_list(db, 'school')
-delete_task(db, 'school', 1)
-puts "---"
-view_list(db, 'school')
-# delete_list(db, 'school')
-# display_lists(db)
+# create_list(db, 'school')
+# #view_list(db, 'school')
+# add_task(db, 'school', 'chemistry')
+# #view_list(db, 'school')
+# edit_list(db, 'school', 3, 'physics')
+# puts "---"
+# view_list(db, 'school')
+# delete_task(db, 'school', 1)
+# puts "---"
+# view_list(db, 'school')
+# # delete_list(db, 'school')
+# # display_lists(db)
 
-# instruction = gets.chomp
-# instruction_arr = get_string_arguments(instruction)
-# allowed_commands = ['CREATE', 'VIEW', 'ADD', 'EDIT', \
-#   'DELETE_LIST', 'DELETE_TASK', 'EXIT']
+instruction = gets.chomp
+instruction_arr = get_string_arguments(instruction)
+allowed_commands = ['CREATE', 'VIEW', 'ADD', 'EDIT', \
+  'DELETE_LIST', 'DELETE_TASK', 'EXIT']
 
-# while instruction_arr[0] != 'EXIT'
-#   #p get_string_arguments(instruction)
-#   if allowed_commands.index(instruction_arr[0]) == nil
-#     puts "======================================"
-#     puts "ERROR: ENTER COMMANDS IN GIVEN FORMAT"
-#     puts "======================================"
-#     puts instruction_list_message
-#   end
-#   instruction = gets.chomp
-#   instruction_arr = get_string_arguments(instruction)
-# end
+while instruction_arr[0] != 'EXIT'
+  #p get_string_arguments(instruction)
+  if allowed_commands.index(instruction_arr[0]) == nil
+    puts "======================================"
+    puts "ERROR: ENTER COMMANDS IN GIVEN FORMAT"
+    puts "======================================"
+    puts instruction_list_message
+  end
+
+  case instruction_arr[0]
+  when 'CREATE'
+    create_list(db, instruction_arr[1])
+  when'VIEW'
+    view_list(db, instruction_arr[1])
+  when 'ADD'
+    add_task(db, instruction_arr[1], instruction_arr[2])
+  when 'EDIT'
+    edit_list(db, instruction_arr[1], instruction_arr[2],\
+      instruction_arr[3])
+  when 'DELETE_LIST'
+    delete_list(db, instruction_arr[1])
+  when 'DELETE_TASK'
+    delete_task(db, instruction_arr[1], instruction_arr[2])
+  end
+
+  instruction = gets.chomp
+  instruction_arr = get_string_arguments(instruction)
+end
 
