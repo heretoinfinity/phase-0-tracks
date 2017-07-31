d2523e16fdde2edeb35505162e45fbec6b8401f9
Thomas <thomasmbise@gmail.com>
2017-07-31 10:37:57 -0400

    Add showing user delete command

--- databases/to_do_list/to_do_list.rb
+++ databases/to_do_list/to_do_list.rb
@@ -7,6 +7,7 @@ CREATE(list_name) - to start a new list called list_name
 VIEW(list_name) - to see tasks in the list including their ids
 ADD(list_name, task_name) - to add task_name to the list
 EDIT(list_name, task_id, new_task_name) - to edit task_name to the list
+DELETE(list_name)
 EXIT - to exit program
 --------------------------------------------------------------------------
 MESSAGE
