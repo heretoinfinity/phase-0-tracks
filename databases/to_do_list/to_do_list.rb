db2dae3587b3e193a18d57ef530818143920e69a
Thomas <thomasmbise@gmail.com>
2017-07-31 10:49:17 -0400

    Change format of instructions & add instruction parser

--- databases/to_do_list/to_do_list.rb
+++ databases/to_do_list/to_do_list.rb
@@ -3,11 +3,28 @@ require 'sqlite3'
 puts <<-MESSAGE
 Commands that can be used in this To-Do list app
 --------------------------------------------------------------------------
-CREATE(list_name) - to start a new list called list_name
-VIEW(list_name) - to see tasks in the list including their ids
-ADD(list_name, task_name) - to add task_name to the list
-EDIT(list_name, task_id, new_task_name) - to edit task_name to the list
-DELETE(list_name)
+CREATE list_name - to start a new list called list_name
+VIEW list_name - to see tasks in the list including their ids
+ADD list_name task_name - to add task_name to the list
+EDIT list_name task_id new_task_name - to edit task_name to the list
+DELETE_LIST list_name
+DELETE_TAST task_name
 EXIT - to exit program
 --------------------------------------------------------------------------
 MESSAGE
+
+db = SQLite3::Database.new("to_do.db")
+
+def get_string_arguments(str)
+  return str.split(" ")
+end
+
+instruction = gets.chomp
+p get_string_arguments(instruction)
+
+create_list_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS kittens(
+    id INTEGER PRIMARY KEY,
+    task VARCHAR(255),
+  )
+SQL
